2ecabc4c75e2f0f04a7cdd1c777c3435dfcd9799
mconsidine <matt@considine.net>
2025-09-24 15:46:32 -0400

    Update setup-dependencies.sh

--- .github/workflows/setup-dependencies.sh
+++ .github/workflows/setup-dependencies.sh
@@ -4,4 +4,5 @@ set -xe
 
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
-sudo apt-get install -y parted pv rsync wget systemd-container qemu-user-static make zip zstd xz-utils wpasupplicant
+sudo apt-get install -y parted pv rsync wget systemd-container qemu-user-static make zip zstd xz-utils 
+sudo apt-get install -y wpasupplicant network-manager
