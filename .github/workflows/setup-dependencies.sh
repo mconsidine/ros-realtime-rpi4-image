03033baf7ffcbda39c995da2e3216a350e917b9a
mconsidine <matt@considine.net>
2025-09-24 15:55:06 -0400

    Update setup-dependencies.sh

--- .github/workflows/setup-dependencies.sh
+++ .github/workflows/setup-dependencies.sh
@@ -6,4 +6,3 @@ export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
 sudo apt-get install -y parted pv rsync wget systemd-container qemu-user-static make zip zstd xz-utils 
 sudo apt-get install -y wpasupplicant network-manager
-sudo useradd -m -G sudo matt && 1234 matt
